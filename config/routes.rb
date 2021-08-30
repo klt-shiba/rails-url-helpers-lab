c98b8065733812a60940ba15a90546f164463c88
Colin Tseng <klt.aus@yahoo.com>
2021-08-30 20:12:54 +1000

    almost there

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'students', to: 'students#index'
+  get 'students/:id', to: 'students#show'
+  get 'students/:id/activate', to: 'students#set_student'
+
+  resources :students, only: [:index, :show]
+
+
 end
